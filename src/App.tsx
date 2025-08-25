61ee20c197a66e1c00a2d606ff80b23e5f0477a3
Rex <rexgreenway@proton.me>
2025-08-25 16:33:52 +0100

    cleaned up version of copilots

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,31 @@
-import * as d3 from "d3";
-import { useEffect, useState } from "react";
-
 import Network from "./components/Network";
-import Simple from "./components/Simple";
-
-import { Node, GetNodesAndLinks } from "./types/node";
 
 // import logo from "/logo.svg";
 
-function App() {
-  const [nodes, setNodes] = useState<Node[]>([]);
-  const [links, setLinks] = useState<d3.SimulationLinkDatum<Node>[]>([]);
+const App = () => {
+  // const [nodes, setNodes] = useState<Node[]>([]);
+  // const [links, setLinks] = useState<d3.SimulationLinkDatum<Node>[]>([]);
 
-  useEffect(() => {
-    d3.csv("/data.csv", (d) => {
-      const node: Node = {
-        id: Number(d.id),
-        name: d.name,
-        picture_url: d.picture_url,
-        team: d.team,
-        parent: Number(d.parent),
-      };
-      return node;
-    })
-      .then((data) => {
-        const [nodes, links] = GetNodesAndLinks(data);
-        setNodes(nodes);
-        setLinks(links);
-      })
-      .catch((error) => {
-        console.error("Error loading data: ", error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   d3.csv("/data.csv", (d) => {
+  //     const node: Node = {
+  //       id: Number(d.id),
+  //       name: d.name,
+  //       picture_url: d.picture_url,
+  //       team: d.team,
+  //       parent: Number(d.parent),
+  //     };
+  //     return node;
+  //   })
+  //     .then((data) => {
+  //       const [nodes, links] = GetNodesAndLinks(data);
+  //       setNodes(nodes);
+  //       setLinks(links);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error loading data: ", error);
+  //     });
+  // }, []);
 
   // console.log("NODES: ", nodes);
   // console.log("LINKS: ", links);
@@ -40,10 +34,9 @@ function App() {
     <>
       <h1>Org Chart</h1>
 
-      {/* <Network data={{ nodes, links }} /> */}
-      <Simple data={{ nodes, links }} />
+      <Network />
     </>
   );
-}
+};
 
 export default App;
--- src/components/Network.tsx
+++ src/components/Network.tsx
@@ -1,22 +1,126 @@
 import * as d3 from "d3";
-import { SimulationLinkDatum } from "d3";
+import { SimulationLinkDatum, SimulationNodeDatum, Simulation } from "d3";
 
 import { useRef, useEffect, useLayoutEffect, useState } from "react";
 
-import { Node } from "../types/node";
+const TEST_IMAGE =
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F2380794%2Fpexels-photo-2380794.jpeg%3Fcs%3Dsrgb%26dl%3Dpexels-kevin-bidwell-2380794.jpg%26fm%3Djpg&f=1&nofb=1&ipt=e28da8ebf542691592482e3345e9067106b14c646134c0d7b58919eb1725df6b&ipo=images";
+
+const NODE_RADIUS = 20;
+
+enum NodeType {
+  Person = "person",
+  Team = "team",
+}
+
+// Person Node cannot have children but has a picture and radius
+type PersonNode = SimulationNodeDatum & {
+  id: string;
+  name: string;
+  type: NodeType.Person;
+  pictureURL: string;
+  // radius is used here as r is already set by the simulation NodeDatum
+  radius: number;
+};
+
+// Team Node can have children and no picture or radius
+type TeamNode = SimulationNodeDatum & {
+  id: string;
+  name: string;
+  type: NodeType.Team;
+  children: PersonNode[];
+
+  childSim?: Simulation<PersonNode, SimulationLinkDatum<PersonNode>>;
+};
+
+type Node = PersonNode | TeamNode;
+
+const TEST_NODES: Node[] = [
+  {
+    id: "1",
+    name: "BIG BOSS",
+    type: NodeType.Person,
+    pictureURL: TEST_IMAGE,
+    radius: NODE_RADIUS,
+  },
+  {
+    id: "2",
+    name: "SUB BOSS",
+    type: NodeType.Person,
+    radius: NODE_RADIUS,
+    pictureURL: TEST_IMAGE,
+  },
+  {
+    id: "3",
+    name: "A-TEAM",
+    type: NodeType.Team,
+    children: [
+      {
+        id: "3-1",
+        name: "TEAM LEAD",
+        type: NodeType.Person,
+        pictureURL: TEST_IMAGE,
+        radius: NODE_RADIUS,
+      },
+      {
+        id: "3-2",
+        name: "TEAM MEMBER",
+        type: NodeType.Person,
+        pictureURL: TEST_IMAGE,
+        radius: NODE_RADIUS,
+      },
+    ],
+  },
+  {
+    id: "4",
+    name: "B-TEAM",
+    type: NodeType.Team,
+    children: [
+      {
+        id: "4-1",
+        name: "HELLO",
+        type: NodeType.Person,
+        pictureURL: TEST_IMAGE,
+        radius: NODE_RADIUS,
+      },
+      {
+        id: "4-2",
+        name: "BANANA",
+        type: NodeType.Person,
+        pictureURL: TEST_IMAGE,
+        radius: NODE_RADIUS,
+      },
+      {
+        id: "4-3",
+        name: "OTHER",
+        type: NodeType.Person,
+        pictureURL: TEST_IMAGE,
+        radius: NODE_RADIUS,
+      },
+      {
+        id: "4-4",
+        name: "OTHER",
+        type: NodeType.Person,
+        pictureURL: TEST_IMAGE,
+        radius: NODE_RADIUS,
+      },
+    ],
+  },
+];
+
+type Link = SimulationLinkDatum<Node>;
+
+const TEST_LINKS: Link[] = [
+  { source: "1", target: "2" },
+  { source: "1", target: "3" },
+  { source: "1", target: "4" },
+];
 
 /**
  * Network ....
  *
  */
-const Network = ({
-  data,
-}: {
-  data: {
-    nodes: Node[];
-    links: SimulationLinkDatum<Node>[];
-  };
-}) => {
+const Network = () => {
   // divRef: references plot's container
   const divRef = useRef(null);
   // svgRef: references the d3 svg (necessary as React and D3 manipulate the DOM)
@@ -30,106 +134,55 @@ const Network = ({
     SetHeight(divRef.current ? divRef.current["offsetHeight"] : 300);
   };
 
-  const color = d3.scaleOrdinal(d3.schemeCategory10);
-
   // Hook watching for window resizing
   useEffect(() => {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   });
 
-  const nodeRadius = 20;
-
-  const TEST_IMAGE =
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F2380794%2Fpexels-photo-2380794.jpeg%3Fcs%3Dsrgb%26dl%3Dpexels-kevin-bidwell-2380794.jpg%26fm%3Djpg&f=1&nofb=1&ipt=e28da8ebf542691592482e3345e9067106b14c646134c0d7b58919eb1725df6b&ipo=images";
-  const testData = {
-    nodes: [
-      {
-        id: "1",
-        name: "BIG BOSS",
-        type: "person",
-        picture_url: TEST_IMAGE,
-      },
-      {
-        id: "2",
-        name: "SUB BOSS",
-        type: "person",
-        picture_url: TEST_IMAGE,
-      },
-      {
-        id: "3",
-        name: "A-TEAM",
-        type: "team",
-        children: [
-          {
-            id: "3-1",
-            name: "TEAM LEAD",
-            type: "person",
-            picture_url: TEST_IMAGE,
-            r: nodeRadius,
-          },
-          {
-            id: "3-2",
-            name: "TEAM MEMBER",
-            type: "person",
-            picture_url: TEST_IMAGE,
-            r: nodeRadius,
-          },
-        ],
-      },
-    ],
-    links: [
-      { source: "1", target: "2" },
-      { source: "1", target: "3" },
-    ],
-  };
-
-  // Render d3 simulation
+  // Nested simulation: outer for top-level nodes, inner for team children
   useLayoutEffect(() => {
     handleResize();
 
-    // The force simulation mutates links and nodes, so create a copy
-    // so that re-evaluating this cell produces the same result.
-    const links: SimulationLinkDatum<Node>[] = testData.links.map((d) => ({
-      ...d,
-    }));
-    const nodes: Node[] = testData.nodes.map((d) => ({ ...d, r: nodeRadius }));
-    console.log("NODES: ", nodes);
+    // COPYING DATA AS D3 MANIPULATES DATA IN PLACE
+    const links = TEST_LINKS.map((d) => ({ ...d }));
+    const nodes = TEST_NODES.map((d) => ({ ...d }));
 
-    // // Add the enclosed circles to the nodes list
-    // // Group nodes into arrays based on the team field
-    // const teamsMap = nodes.reduce((acc, node) => {
-    //   const team = node.team;
-    //   if (team) {
-    //     if (!acc[team]) acc[team] = [];
-    //     acc[team].push(node);
-    //   }
-    //   return acc;
-    // }, {} as Record<string, Node[]>);
-
-    // console.log("TEAMS MAP: ", teamsMap);
-
-    // Create simulation
+    // Outer simulation for top-level nodes
     const simulation = d3
       .forceSimulation(nodes)
       .force(
         "link",
-        d3.forceLink<Node, SimulationLinkDatum<Node>>(links).id((d) => d.id)
+        d3.forceLink<Node, Link>(links).id((d) => d.id)
       )
-      .force(
-        "charge",
-        d3.forceManyBody<Node>().strength((d) => d.r)
-      )
+      .force("charge", d3.forceManyBody<Node>())
+      // Colliding force between nodes but
       .force(
         "collide",
         d3.forceCollide<Node>(
           (d) =>
-            (d.type === "team" ? d.children?.length * nodeRadius : nodeRadius) +
-            10
+            (d.type === NodeType.Team
+              ? d.children.length * NODE_RADIUS
+              : NODE_RADIUS) + 20
         )
       )
-      .force("x", d3.forceX())
-      .force("y", d3.forceY());
+      // Use forceCenter to keep the simulation centered at (0,0)
+      .force("center", d3.forceCenter<Node>(0, 0));
+
+    // For each team, create a simulation for its children
+    nodes.forEach((n) => {
+      if (n.type === NodeType.Team) {
+        // Adding r here is necessary for this to work....?
+        n.children = n.children.map((d) => ({ ...d, r: d.radius }));
+        const childSim = d3
+          .forceSimulation(n.children)
+          .force("x", d3.forceX(0))
+          .force("y", d3.forceY(0))
+          .force("collide", d3.forceCollide(NODE_RADIUS + 2))
+          .stop();
+        for (let i = 0; i < 60; ++i) childSim.tick();
+      }
+    });
 
     // Select the svg
     const svg = d3
@@ -139,141 +192,128 @@ const Network = ({
       .attr("viewBox", [-width / 2, -height / 2, width, height])
       .attr("style", "max-width: 100%; height: auto;");
 
-    // Select existing <g> or append if not present
-    let linkGroup = svg.select("g.links");
-    if (linkGroup.empty()) {
-      linkGroup = svg.append("g").attr("class", "links");
-    }
-
-    // Select existing <g> or append if not present
-    let nodeGroup = svg.select("g.nodes");
-    if (nodeGroup.empty()) {
-      nodeGroup = svg.append("g").attr("class", "nodes");
-    }
-
-    // Add a line for each link, and a circle for each node.
-    const link = linkGroup
+    // Add a line for each link
+    const link = svg
       .selectAll("line")
-      .data(links)
+      .data<Link>(links)
       .join("line")
       .attr("stroke", "#999")
       .attr("stroke-opacity", 0.6)
-      .attr("stroke-width", Math.sqrt(2));
+      .attr("stroke-width", 2);
 
     // Define patterns for each node with an image
+    svg.select("defs").remove();
     const defs = svg.append("defs");
-    nodes.forEach((d, i) => {
-      if (d.picture_url) {
+    nodes.forEach((d) => {
+      // Each person node -> create an image pattern
+      if (d.type === NodeType.Person) {
         defs
           .append("pattern")
           .attr("id", `node-image-${d.id}`)
-          .attr("patternUnits", "objectBoundingBox")
-          .attr("width", nodeRadius * 2)
-          .attr("height", nodeRadius * 2)
+          .attr("width", NODE_RADIUS * 2)
+          .attr("height", NODE_RADIUS * 2)
           .append("image")
-          .attr("href", d.picture_url)
-          .attr("width", nodeRadius * 2)
-          .attr("height", nodeRadius * 2)
-          .attr("x", 0)
-          .attr("y", 0);
+          .attr("href", d.pictureURL)
+          .attr("width", NODE_RADIUS * 2)
+          .attr("height", NODE_RADIUS * 2);
+      }
+      if (d.type === NodeType.Team) {
+        d.children.forEach((c) => {
+          defs
+            .append("pattern")
+            .attr("id", `node-image-${c.id}`)
+            .attr("width", NODE_RADIUS * 2)
+            .attr("height", NODE_RADIUS * 2)
+            .append("image")
+            .attr("href", c.pictureURL)
+            .attr("width", NODE_RADIUS * 2)
+            .attr("height", NODE_RADIUS * 2);
+        });
       }
     });
 
-    const node = nodeGroup
+    // Join <g> for each node
+    const node = svg
       .selectAll("g.node")
-      .data(nodes)
-      .join((enter) => {
-        const g = enter.append("g").attr("class", "node");
-        // Conditionally append circles
-        g.each(function (d) {
-          const group = d3.select(this);
-          console.log("d:", d);
-          if (d.children) {
-            group
-              .selectAll("circle")
-              .data(d.children)
-              .join("circle")
-              .attr("r", 20);
-          } else {
-            group.append("circle").attr("r", 20);
-          }
-        });
-        return g;
-      });
+      .data<Node>(nodes)
+      .join("g")
+      .attr("class", "node");
 
-    // const node = nodeGroup
-    //   .selectAll("g")
-    //   .data(nodes)
-    //   .join((enter) =>
-    //     enter
-    //       .append("g")
-    //       .attr("class", "node")
-    //       .attr("type", (d) => d.type)
-    //       .selectAll("circle")
-    //       .data((d) => [d])
-    //       // .data((d) => {
-    //       //   const people = d.children?.concat(d) || [d];
+    // For each node, update its children or self
+    node.each(function (d: Node) {
+      const group = d3.select(this);
+      group.selectAll("circle.team-bound").remove();
+      group.selectAll("text.team-label").remove();
+      if (d.type === NodeType.Team) {
+        // Compute bounding radius for children
+        const boundingRadius = d3.packEnclose(d.children).r + 6;
 
-    //       //   // // what is the pack layout
-    //       //   // if (d.children) {
-    //       //   //   const root = d3.hierarchy(d);
-    //       //   //   console.log("ROOT: ", root);
-    //       //   //   const packFunc = d3
-    //       //   //     .pack()
-    //       //   //     .size([nodeRadius * 2, nodeRadius * 2])
-    //       //   //     .padding(3);
-    //       //   //   const parentCircle = packFunc(root);
-    //       //   //   console.log("PARENT CIRCLE: ", parentCircle);
-    //       //   // }
+        // Draw white BOUNDING CIRCLE first
+        group
+          .append("circle")
+          .attr("class", "team-bound")
+          .attr("r", boundingRadius)
+          .attr("fill", "#fff")
+          .attr("cx", 0)
+          .attr("cy", 0);
 
-    //       //   return people;
-    //       // })
-    //       .join((enter) => {
-    //         console.log("2.ENTER: ", enter);
-    //         console.log("HERE: ", enter.data());
-    //         return enter
-    //           .append("circle")
-    //           .attr("name", (d) => d.name)
-    //           .attr("fill", (d) =>
-    //             d.picture_url ? `url(#node-image-${d.id})` : "none"
-    //           )
-    //           .attr("r", (d) => {
-    //             console.log("D: ", d);
-    //             return d.type === "team"
-    //               ? d.children?.length * nodeRadius
-    //               : nodeRadius;
-    //           })
-    //           .attr("stroke", "#e70000ff")
-    //           .attr("stroke-width", 1.5);
-    //       })
-    //   );
+        // Draw TEAM NAME label above the bounding circle
+        group
+          .append("text")
+          .attr("class", "team-label")
+          .attr("x", 0)
+          .attr("y", -boundingRadius - 5)
+          .attr("text-anchor", "middle")
+          .attr("font-size", 18)
+          .attr("font-family", "sans-serif")
+          .attr("fill", "#333")
+          .text(d.name || "");
 
-    // const node = nodeGroup
-    //   .selectAll("circle")
-    //   .data(nodes)
-    //   .join("circle")
-    //   .attr("stroke", "#e70000ff")
-    //   .attr("stroke-width", 1.5)
-    //   .attr("r", nodeRadius)
-    //   .attr("team", (d) => d.team || "default")
-    //   .attr("fill", (d) =>
-    //     d.picture_url ? `url(#node-image-${d.id})` : "none"
-    //   );
+        // Draw bubble children
+        group
+          .selectAll("circle.child")
+          .data(d.children)
+          .join("circle")
+          .attr("class", "child")
+          .attr("r", (d) => d.radius)
+          .attr("fill", (c) =>
+            c.pictureURL ? `url(#node-image-${c.id})` : "#eee"
+          )
+          .attr("stroke", "#e70000ff")
+          .attr("stroke-width", 1.5)
+          .attr("cx", (c) => c.x!)
+          .attr("cy", (c) => c.y!)
+          .raise();
+      } else {
+        group
+          .selectAll("circle.child")
+          .data([d])
+          .join("circle")
+          .attr("class", "child")
+          .attr("r", (d) => d.radius)
+          .attr("fill", d.pictureURL ? `url(#node-image-${d.id})` : "#eee")
+          .attr("stroke", "#e70000ff")
+          .attr("stroke-width", 1.5);
+      }
+    });
 
-    // Set the position attributes of links and nodes each time the simulation ticks.
+    // On each tick, update positions
     simulation.on("tick", () => {
       link
-        .attr("x1", (d) => d.source.x)
-        .attr("y1", (d) => d.source.y)
-        .attr("x2", (d) => d.target.x)
-        .attr("y2", (d) => d.target.y);
+        .attr("x1", (d) => (d.source as Node).x ?? 0)
+        .attr("y1", (d) => (d.source as Node).y ?? 0)
+        .attr("x2", (d) => (d.target as Node).x ?? 0)
+        .attr("y2", (d) => (d.target as Node).y ?? 0);
 
-      node.attr("x", (d) => d.x).attr("y", (d) => d.y);
+      node.attr("transform", (d) => `translate(${d.x},${d.y})`);
     });
-  }, [data, height, width]);
+  }, [height, width]);
 
   return (
-    <div style={{ height: "80vh", border: "solid black 2px" }} ref={divRef}>
+    <div
+      style={{ height: "80vh", border: "solid black 2px", background: "#666" }}
+      ref={divRef}
+    >
       <svg className="m-auto" ref={svgRef} />
     </div>
   );
